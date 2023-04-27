c3c96d9f92acf3489ed1d582b7af4ef9c2c4b0db
Diegopie <ishtaracademy@outlook.com>
2023-04-26 21:11:46 -0600

    Copy clipboard but will show UI element to show that copy was successfull

--- client/src/components/FormInputs.tsx
+++ client/src/components/FormInputs.tsx
@@ -11,6 +11,13 @@ const FormInput = (props: IFormInputProps) => {
         e.preventDefault();
         try {
             await navigator.clipboard.writeText(props.state.stateValue);
+
+            document.getElementById(props.placeholder).classList.remove('hidden');
+
+            setTimeout(() => {
+                document.getElementById(props.placeholder).classList.add('hidden')
+            }, 2500);
+
             console.log(`${props.state.stateValue} copied to clipboard`);
         } catch (err) {
             console.error('Failed to copy: ', err);
@@ -31,11 +38,15 @@ const FormInput = (props: IFormInputProps) => {
             />
             {
                 props.state.stateValue === '' ? null :
-                <button title={`Copy ${props.placeholder} to Clipboard`} className="ml-2 p-1"
-                    onClick={handleClick}
-                >
-                    <img src={'assets/clipboard.png'} className="h-8" />
-                </button>
+                    <article className="relative">
+                        <button title={`Copy ${props.placeholder} to Clipboard`} className="ml-2 p-1"
+                            onClick={handleClick}
+                        >
+                            <img src={'assets/clipboard.png'} className="h-7" />
+                        </button>
+                        <div id={props.placeholder} className="
+                        absolute right-0 top-0 left-9 border  bg-white rounded-md offset-1 w-16 text-center hidden">Copied!</div>
+                    </article>
             }
         </div>
     )
